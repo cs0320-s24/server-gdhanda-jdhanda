e323447ec62d0cd9b7cd9587c41966f3356cfa1d
Gavin Dhanda <gavin_dhanda@brown.edu>
2024-02-14 17:43:26 -0500

    chore: Added inline comments to BroadbandHandler, and re-made the CensusAPIUtilities class for querying the census.

--- src/main/java/edu/brown/cs/student/main/server/handlers/census/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/handlers/census/BroadbandHandler.java
@@ -1,20 +1,14 @@
 package edu.brown.cs.student.main.server.handlers.census;
 
-import com.squareup.moshi.JsonAdapter;
-import com.squareup.moshi.Moshi;
-import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.CountyNotFoundException;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.DatasourceException;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.StateNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import okio.Buffer;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -35,23 +29,27 @@ public class BroadbandHandler implements Route {
   @Override
   public Object handle(Request request, Response response) {
 
-    // store the state and county of the request
+    // Store the state and county of the request.
     String state = request.queryParams("state");
     String county = request.queryParams("county");
 
+    // Initialize the response format.
     Map<String, Object> responseData = new HashMap<>();
     try {
+      // Check the cache for requested data, otherwise query the census.
       CensusData censusData;
       if ((censusData = this.censusCache.get((state + county))) == null) {
         censusData = this.censusRequest(state, county);
         this.censusCache.put((state + county), censusData);
       }
 
+      // Add relevant fields to the result.
       responseData.put("result", "success");
       responseData.put("time", getTime());
       responseData.put("broadband", censusData);
 
     } catch (Exception e) {
+      // Add descriptive error message to the result.
       e.printStackTrace();
       responseData.put("result", "error");
       responseData.put("caused by", e.getMessage());
@@ -62,50 +60,63 @@ public class BroadbandHandler implements Route {
   private CensusData censusRequest(String state, String county)
       throws IOException, StateNotFoundException, CountyNotFoundException, DatasourceException {
 
+    // Get state and county code, and use them to query the census.
     List<String> codes = this.getCodes(state, county);
     List<List<String>> results =
-        this.queryCensus(
+        CensusAPIUtilities.queryCensus(
             "/data/2021/acs/acs1/subject/variables?"
                 + "get=NAME,S2802_C03_022E&for=county:"
                 + codes.get(1)
                 + "&in=state:"
                 + codes.get(0));
 
+    // Construct a CensusData object with the results and return it.
     return new CensusData(state, county, results.get(1).get(1));
   }
 
   private List<String> getCodes(String state, String county)
       throws IOException, StateNotFoundException, CountyNotFoundException, DatasourceException {
+
+    // Check and load state codes.
     if (!this.haveStateCodes) {
       this.fetchAllStateCodes();
       this.haveStateCodes = true;
     }
 
+    // Get State code from stateCodes instance variable.
     if (!this.stateCodes.containsKey(state)) {
       throw new StateNotFoundException("Could not find state: " + state);
     }
     String stateCode = this.stateCodes.get(state);
 
+    // Use CensusAPIUtilities to query the census.
     List<List<String>> counties =
-        this.queryCensus("/data/2010/dec/sf1?get=NAME&for=county:*&in=state:" + stateCode);
+        CensusAPIUtilities.queryCensus(
+            "/data/2010/dec/sf1?get=NAME&for=county:*&in=state:" + stateCode);
 
+    // Look for the county with the corresponding name.
     String countyCode = "";
-    String countyState = county + ", " + state;
     for (List<String> list : counties) {
-      if (list.get(0).equals(countyState)) {
+      if (list.get(0).equals(county + ", " + state)) {
         countyCode = list.get(2);
       }
     }
+
+    // Make sure county was found.
     if (!(countyCode.length() > 0)) {
       throw new CountyNotFoundException("Could not find county: " + county);
     }
 
+    // Return a list of the county and state codes.
     return List.of(stateCode, countyCode);
   }
 
   private void fetchAllStateCodes() throws IOException, DatasourceException {
-    List<List<String>> results = this.queryCensus("/data/2010/dec/sf1?get=NAME&for=state:*");
+    // Query the census for the state codes.
+    List<List<String>> results =
+        CensusAPIUtilities.queryCensus("/data/2010/dec/sf1?get=NAME&for=state:*");
 
+    // Convert the list to an instance variable hashmap and remove the header.
     for (List<String> list : results) {
       this.stateCodes.put(list.get(0), list.get(1));
     }
@@ -116,36 +127,5 @@ public class BroadbandHandler implements Route {
     LocalDateTime dateAndTime = LocalDateTime.now();
     DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     return dateAndTime.format(format);
-  }
-
-  private List<List<String>> queryCensus(String file) throws IOException, DatasourceException {
-    URL requestURL = new URL("https", "api.census.gov", file);
-    HttpURLConnection clientConnection = this.connect(requestURL);
-    Moshi moshi = new Moshi.Builder().build();
-
-    Type listListString = Types.newParameterizedType(List.class, List.class, String.class);
-    JsonAdapter<List<List<String>>> adapter = moshi.adapter(listListString);
-
-    List<List<String>> results =
-        adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
-    clientConnection.disconnect();
-
-    return results;
-  }
-
-  /**
-   * Private helper method; throws IOException so different callers can handle differently if
-   * needed.
-   */
-  private static HttpURLConnection connect(URL requestURL) throws DatasourceException, IOException {
-    URLConnection urlConnection = requestURL.openConnection();
-    if (!(urlConnection instanceof HttpURLConnection))
-      throw new DatasourceException("unexpected: result of connection wasn't HTTP");
-    HttpURLConnection clientConnection = (HttpURLConnection) urlConnection;
-    clientConnection.connect(); // GET
-    if (clientConnection.getResponseCode() != 200)
-      throw new DatasourceException(
-          "unexpected: API connection not success status " + clientConnection.getResponseMessage());
-    return clientConnection;
   }
 }
---
+++ src/main/java/edu/brown/cs/student/main/server/handlers/census/CensusAPIUtilities.java
@@ -0,0 +1,48 @@
+package edu.brown.cs.student.main.server.handlers.census;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import edu.brown.cs.student.main.server.handlers.census.exceptions.DatasourceException;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import okio.Buffer;
+
+public class CensusAPIUtilities {
+
+  public static List<List<String>> queryCensus(String file)
+      throws IOException, DatasourceException {
+    URL requestURL = new URL("https", "api.census.gov", file);
+    HttpURLConnection clientConnection = connect(requestURL);
+    Moshi moshi = new Moshi.Builder().build();
+
+    Type listListString = Types.newParameterizedType(List.class, List.class, String.class);
+    JsonAdapter<List<List<String>>> adapter = moshi.adapter(listListString);
+
+    List<List<String>> results =
+        adapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+    clientConnection.disconnect();
+
+    return results;
+  }
+
+  /**
+   * Private helper method; throws IOException so different callers can handle differently if
+   * needed.
+   */
+  private static HttpURLConnection connect(URL requestURL) throws DatasourceException, IOException {
+    URLConnection urlConnection = requestURL.openConnection();
+    if (!(urlConnection instanceof HttpURLConnection))
+      throw new DatasourceException("unexpected: result of connection wasn't HTTP");
+    HttpURLConnection clientConnection = (HttpURLConnection) urlConnection;
+    clientConnection.connect();
+    if (clientConnection.getResponseCode() != 200)
+      throw new DatasourceException(
+          "unexpected: API connection not success status " + clientConnection.getResponseMessage());
+    return clientConnection;
+  }
+}
