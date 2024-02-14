e3bab563323283de93e859a32f597fe143446d4d
Julian5125 <Julian_dhanda@brown.edu>
2024-02-13 21:02:34 -0500

    pair-prog w/ @gdhanda: Added StateNotFoundException to git.

---
+++ src/main/java/edu/brown/cs/student/main/server/handlers/census/StateNotFoundException.java
@@ -0,0 +1,8 @@
+package edu.brown.cs.student.main.server.handlers.census;
+
+public class StateNotFoundException extends Exception {
+
+  public StateNotFoundException(String message) {
+    super(message);
+  }
+}
