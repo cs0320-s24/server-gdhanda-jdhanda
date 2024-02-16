12935d099d71b9ebdd248e5c9b8dfed1bd58dd80
Gavin Dhanda <gavin_dhanda@brown.edu>
2024-02-16 18:04:33 -0500

    feat: test added for basic cache datasource.

--- src/test/java/edu/brown/cs/student/census/TestBroadbandSource.java
+++ src/test/java/edu/brown/cs/student/census/TestBroadbandSource.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import edu.brown.cs.student.main.server.handlers.census.BroadbandDatasource;
 import edu.brown.cs.student.main.server.handlers.census.CensusAPISource;
 import edu.brown.cs.student.main.server.handlers.census.CensusData;
+import edu.brown.cs.student.main.server.handlers.census.caching.CachingCensusSource;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.CountyNotFoundException;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.DatasourceException;
 import edu.brown.cs.student.main.server.handlers.census.exceptions.StateNotFoundException;
@@ -23,6 +24,24 @@ public class TestBroadbandSource {
   public void testCensusCanLoad_REAL()
       throws DatasourceException, StateNotFoundException, IOException, CountyNotFoundException {
     BroadbandDatasource source = new CensusAPISource();
+    CensusData res = source.getBroadbandData("Colorado", "Denver County");
+    assertNotNull(res);
+
+    assertEquals(res.name(), "Denver County, Colorado");
+    float broadband = Float.parseFloat(res.broadband().substring(0, res.broadband().length() - 1));
+    assertTrue(broadband >= 0);
+    assertTrue(broadband <= 100);
+  }
+
+  /**
+   * This method tests the real census datasource using the cache.
+   *
+   * @throws DatasourceException
+   */
+  @Test
+  public void testCensusCacheCanLoad_REAL()
+      throws DatasourceException, StateNotFoundException, IOException, CountyNotFoundException {
+    BroadbandDatasource source = new CachingCensusSource(new CensusAPISource());
     CensusData res = source.getBroadbandData("Colorado", "Denver County");
     assertNotNull(res);
 
