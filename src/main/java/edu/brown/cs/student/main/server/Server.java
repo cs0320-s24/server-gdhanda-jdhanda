49069116153126c653f1e0e7cc7f3ade67873acb
Julian5125 <Julian_dhanda@brown.edu>
2024-02-16 12:44:51 -0500

    docs: Commented Server.

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -13,25 +13,44 @@ import edu.brown.cs.student.main.server.handlers.csvhandlers.SearchCSVHandler;
 import edu.brown.cs.student.main.server.handlers.csvhandlers.ViewCSVHandler;
 import spark.Spark;
 
+/**
+ * The main Server class, it starts the server, handles the endpoints, and sets access to the server.
+ */
 public class Server {
 
-  private CSVDatasource csvSource;
-  private BroadbandDatasource broadbandSource;
+  private CSVDatasource csvSource; // Will handle the csv data.
+  private BroadbandDatasource broadbandSource; // Will handle the Census API data.
 
+  /**
+   * Initializes instance variables for the csv source and broadband source and calls the run method.
+   *
+   * @param csvSource will handle the csv data.
+   * @param broadbandSource will handle the Census API data.
+   */
   private Server(CSVDatasource csvSource, BroadbandDatasource broadbandSource) {
     this.csvSource = csvSource;
     this.broadbandSource = broadbandSource;
     this.run();
   }
 
+  /**
+   * Main method creates a server.
+   *
+   * @param args is unused.
+   */
   public static void main(String[] args) {
     new Server(new CSVSharedSource(), new CachingCensusSource(new CensusAPISource()));
   }
 
+  /**
+   * Starts the server on port 3232, sets access control, assigns handlers to the endpoints, and
+   * notifies the terminal of success.
+   */
   private void run() {
     int port = 3232;
     Spark.port(port);
 
+    // Only allow access from certain addresses.
     after(
         (request, response) -> {
           response.header("Access-Control-Allow-Origin", "http://localhost:3232");
