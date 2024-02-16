b69ba1592f55db9b91e5d4f456db6bb15fd5294b
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2024-02-16 17:20:03 -0500

    feat: added testing for real census query.

---
+++ src/test/java/edu/brown/cs/student/census/TestBroadbandSource.java
@@ -0,0 +1,34 @@
+package edu.brown.cs.student.census;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.brown.cs.student.main.server.handlers.census.BroadbandDatasource;
+import edu.brown.cs.student.main.server.handlers.census.CensusAPISource;
+import edu.brown.cs.student.main.server.handlers.census.CensusData;
+import edu.brown.cs.student.main.server.handlers.census.exceptions.CountyNotFoundException;
+import edu.brown.cs.student.main.server.handlers.census.exceptions.DatasourceException;
+import edu.brown.cs.student.main.server.handlers.census.exceptions.StateNotFoundException;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+/** UNIT tests for the Census API datasource class. */
+public class TestBroadbandSource {
+
+  /**
+   * This method tests the real census datasource.
+   *
+   * @throws DatasourceException
+   */
+  @Test
+  public void testCensusCanLoad_REAL()
+      throws DatasourceException, StateNotFoundException, IOException, CountyNotFoundException {
+    BroadbandDatasource source = new CensusAPISource();
+    CensusData res = source.getBroadbandData("Colorado", "Denver County");
+    assertNotNull(res);
+
+    assertEquals(res.name(), "Denver County, Colorado");
+    float broadband = Float.parseFloat(res.broadband().substring(0, res.broadband().length() - 1));
+    assertTrue(broadband >= 0);
+    assertTrue(broadband <= 100);
+  }
+}
