77d7c1bff9ca8dee03cf054a9302fcce879cd79b
gdhanda26 <113314719+gdhanda26@users.noreply.github.com>
2024-02-16 16:49:17 -0500

    refactor: moved mock tests to new file.

--- src/test/java/edu/brown/cs/student/census/mocking/MockCensusTests.java
+++ src/test/java/edu/brown/cs/student/census/mocking/MockCensusTests.java
@@ -1,5 +1,151 @@
 package edu.brown.cs.student.census.mocking;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import edu.brown.cs.student.census.mocking.MockCensusSource;
+import edu.brown.cs.student.main.server.handlers.census.BroadbandHandler;
+import edu.brown.cs.student.main.server.handlers.census.CensusData;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import okio.Buffer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeClass;
+import spark.Spark;
+
 public class MockCensusTests {
-  // TODO: Test mock census response.
+
+  @BeforeClass
+  public static void setupOnce() {
+    // Pick an arbitrary free port
+    Spark.port(0);
+    // Eliminate logger spam in console for test suite
+    Logger.getLogger("").setLevel(Level.WARNING); // empty name = root
+  }
+
+  // Helping Moshi serialize Json responses.
+  private final Type mapStringObject =
+      Types.newParameterizedType(Map.class, String.class, Object.class);
+  private JsonAdapter<Map<String, Object>> adapter;
+  private JsonAdapter<CensusData> censusDataAdapter;
+
+  @BeforeEach
+  public void setup() {
+    // Use *MOCKED* data when in this test environment.
+    MockCensusSource mockedSource =
+        new MockCensusSource(
+            new CensusData("Denver County, Colorado", "81", "01", "12", "TIME"),
+            "Colorado",
+            "Denver County");
+    Spark.get("/broadband", new BroadbandHandler(mockedSource));
+    Spark.awaitInitialization(); // don't continue until the server is listening
+
+    // New Moshi adapter for responses and requests
+    Moshi moshi = new Moshi.Builder().build();
+    adapter = moshi.adapter(mapStringObject);
+    censusDataAdapter = moshi.adapter(CensusData.class);
+  }
+
+  @AfterEach
+  public void tearDown() {
+    // Gracefully stop Spark listening on both endpoints
+    Spark.unmap("/broadband");
+    Spark.awaitStop(); // don't proceed until the server is stopped
+  }
+
+  /**
+   * Helper to start a connection to a specific API endpoint/params
+   *
+   * <p>The "throws" clause doesn't matter below -- JUnit will fail if an exception is thrown that
+   * hasn't been declared as a parameter to @Test.
+   *
+   * @param apiCall the call string, including endpoint (Note: this would be better if it had more
+   *     structure!)
+   * @return the connection for the given URL, just after connecting
+   * @throws IOException if the connection fails for some reason
+   */
+  private HttpURLConnection tryRequest(String apiCall) throws IOException {
+    // Configure the connection (but don't actually send a request yet)
+    URL requestURL = new URL("http://localhost:" + Spark.port() + "/" + apiCall);
+    HttpURLConnection clientConnection = (HttpURLConnection) requestURL.openConnection();
+    // The request body contains a Json object
+    clientConnection.setRequestProperty("Content-Type", "application/json");
+    // We're expecting a Json object in the response body
+    clientConnection.setRequestProperty("Accept", "application/json");
+
+    clientConnection.connect();
+    return clientConnection;
+  }
+
+  @Test
+  public void testCensusRequestSuccess() throws IOException {
+    // Set up the request, make the request
+    HttpURLConnection loadConnection = tryRequest("broadband?state=Colorado&county=Denver+County");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, loadConnection.getResponseCode());
+    // Get the expected response: a success
+    Map<String, Object> responseBody =
+        adapter.fromJson(new Buffer().readFrom(loadConnection.getInputStream()));
+    showDetailsIfError(responseBody);
+    assertEquals("success", responseBody.get("result"));
+
+    // Mocked data: correct because we mocked.
+    assertEquals(
+        censusDataAdapter.toJson(
+            new CensusData("Denver County, Colorado", "81", "01", "12", "TIME")),
+        responseBody.get("data"));
+
+    loadConnection.disconnect();
+  }
+
+  @Test
+  public void testCensusRequestFail_Missing() throws IOException {
+    // Setup without any parameters (oops!)
+    HttpURLConnection loadConnection = tryRequest("broadband");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, loadConnection.getResponseCode());
+
+    // Get the expected response: an error
+    Map<String, Object> responseBody =
+        adapter.fromJson(new Buffer().readFrom(loadConnection.getInputStream()));
+    showDetailsIfError(responseBody);
+    assertEquals("error", responseBody.get("result"));
+    loadConnection.disconnect(); // close gracefully
+  }
+
+  @Test
+  public void testCensusRequestFail_BadParams() throws IOException {
+    // Setup with bad parameters (oops)
+    HttpURLConnection loadConnection = tryRequest("broadband?state=MURICA&county=ASIA");
+    // Get an OK response (the *connection* worked, the *API* provides an error response)
+    assertEquals(200, loadConnection.getResponseCode());
+
+    // Get the expected response: an error
+    Map<String, Object> responseBody =
+        adapter.fromJson(new Buffer().readFrom(loadConnection.getInputStream()));
+    showDetailsIfError(responseBody);
+    assertEquals("error", responseBody.get("result"));
+    loadConnection.disconnect(); // close gracefully
+  }
+
+  /**
+   * Helper to make working with a large test suite easier: if an error, print more info.
+   *
+   * @param body
+   */
+  private void showDetailsIfError(Map<String, Object> body) {
+    if (body.containsKey("result") && "error".equals(body.get("result"))) {
+      System.out.println(body);
+    }
+  }
 }
+
