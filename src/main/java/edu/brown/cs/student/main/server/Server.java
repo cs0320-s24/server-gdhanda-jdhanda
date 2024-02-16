a208a42c6582027e897c51dbd5a6b1ac713d0cc1
Gavin Dhanda <gavin_dhanda@brown.edu>
2024-02-16 04:01:17 -0500

    pair-prog w/ @jdhanda: Specified Access-Control in Server and added more specific instructions in terminal...

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -13,8 +13,6 @@ import edu.brown.cs.student.main.server.handlers.csvhandlers.SearchCSVHandler;
 import edu.brown.cs.student.main.server.handlers.csvhandlers.ViewCSVHandler;
 import spark.Spark;
 
-// TODO: FIX Access Control
-
 public class Server {
 
   private CSVDatasource csvSource;
@@ -33,28 +31,11 @@ public class Server {
   private void run() {
     int port = 3232;
     Spark.port(port);
-    /*
 
-       Setting CORS headers to allow cross-origin requests from the client; this is necessary for the client to
-       be able to make requests to the server.
-
-       By setting the Access-Control-Allow-Origin header to "*", we allow requests from any origin.
-       This is not a good idea in real-world applications, since it opens up your server to cross-origin requests
-       from any website. Instead, you should set this header to the origin of your client, or a list of origins
-       that you trust.
-
-       By setting the Access-Control-Allow-Methods header to "*", we allow requests with any HTTP method.
-       Again, it's generally better to be more specific here and only allow the methods you need, but for
-       this demo we'll allow all methods.
-
-       We recommend you learn more about CORS with these resources:
-           - https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-           - https://portswigger.net/web-security/cors
-    */
     after(
         (request, response) -> {
-          response.header("Access-Control-Allow-Origin", "*");
-          response.header("Access-Control-Allow-Methods", "*");
+          response.header("Access-Control-Allow-Origin", "http://localhost:3232");
+          response.header("Access-Control-Allow-Methods", "GET");
         });
 
     // Setting up the handler for the GET /____csv and /broadband endpoints
@@ -66,5 +47,7 @@ public class Server {
     Spark.awaitInitialization();
 
     System.out.println("Server started at http://localhost:" + port);
+    System.out.println(
+        "Valid endpoints are \"loadcsv\", \"searchcsv\", " + "\"viewcsv\", and \"broadband\"!");
   }
 }
